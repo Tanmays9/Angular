c00bb3b63e7997c74af8b792580265a35ec43df6
Tanmay sharma <tanmays040@gmail.com>
2020-07-16 08:47:12 +0530

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,9 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { from } from 'rxjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
@@ -32,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule
     
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
